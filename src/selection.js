f9b16da017d5c43d6dcf6d9a30edf3e513fff58a
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-05-11 19:03:20 -0400

    Cleaned up spacing

--- src/selection.js
+++ src/selection.js
@@ -1,24 +1,24 @@
 export function selectionsort(arr, cmp = (a, b) => a - b){
-	if (!arr.length || arr.length == 1) {
-		return arr;
-	}
-	for (let i = 0; i < arr.length - 1; i++) {
-		let iMin = i;
-		for (let j = i + 1; j < arr.length; j++) {
-			if (cmp(arr[j], arr[iMin]) < 0) {
-				iMin = j;
-			}
-		}
-		if (iMin !== i) {
-			_swap(arr, i, iMin);
-		}
-	}
+  if (!arr.length || arr.length == 1) {
+    return arr;
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
+    let iMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (cmp(arr[j], arr[iMin]) < 0) {
+        iMin = j;
+      }
+    }
+    if (iMin !== i) {
+      _swap(arr, i, iMin);
+    }
+  }
 
-	return arr;
+  return arr;
 }
 
 function _swap(arr, i, j){
-	var temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
+  var temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
 }
--- test/test.js
+++ test/test.js
@@ -12,14 +12,14 @@ var people = [
 ];
 
 function _makeRandomArray(options = {precision: 0, multiplier: 100, size: 100}) {
-	var {precision, multiplier, size} = options;
-	var result = [];
+  var {precision, multiplier, size} = options;
+  var result = [];
 
-	for (let i = size; i > 0; i -= 1) {
-		result.push(parseFloat((Math.random() * multiplier).toFixed(precision)));
-	}
+  for (let i = size; i > 0; i -= 1) {
+    result.push(parseFloat((Math.random() * multiplier).toFixed(precision)));
+  }
 
-	return result;
+  return result;
 }
 
 function desc(a, b) {
@@ -32,32 +32,32 @@ function objComparator(a, b) {
 
 function test(sortType) {
 
-	it('should work with an empty array', () => {
-		sortType([]).should.eql([]);
-	});
+  it('should work with an empty array', () => {
+    sortType([]).should.eql([]);
+  });
 
-	it('should work with (ascending) sorted arrays', () => {
-		sortType([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]).should.eql([1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
-		sortType([9, 2, 8, 6, 1, 3]).should.eql([1, 2, 3, 6, 8, 9]);
-		sortType([5, 2, 2, 6, 1, 3]).should.eql([1, 2, 2, 3, 5, 6]);
-		sortType([5, 2, 4, 6, 1, 3]).should.eql([1, 2, 3, 4, 5, 6]);
-		sortType([0, 0, 0, 0, 0, -1]).should.eql([-1, 0, 0, 0, 0, 0]);
-	});
+  it('should work with (ascending) sorted arrays', () => {
+    sortType([3, 1, 4, 1, 5, 9, 2, 6, 5, 4]).should.eql([1, 1, 2, 3, 4, 4, 5, 5, 6, 9]);
+    sortType([9, 2, 8, 6, 1, 3]).should.eql([1, 2, 3, 6, 8, 9]);
+    sortType([5, 2, 2, 6, 1, 3]).should.eql([1, 2, 2, 3, 5, 6]);
+    sortType([5, 2, 4, 6, 1, 3]).should.eql([1, 2, 3, 4, 5, 6]);
+    sortType([0, 0, 0, 0, 0, -1]).should.eql([-1, 0, 0, 0, 0, 0]);
+  });
 
-	it('should work with (descending) sorted arrays', () => {
-		sortType([4, 2, 2, 6, 1, 3], desc).should.eql([6, 4, 3, 2, 2, 1]);
-		sortType([0, 0, 0, 0, 0, 1], desc).should.eql([1, 0, 0, 0, 0, 0]);
-		sortType([0, 0, 0, 0, 0, -1], desc).should.eql([0, 0, 0, 0, 0, -1]);
-	});
+  it('should work with (descending) sorted arrays', () => {
+    sortType([4, 2, 2, 6, 1, 3], desc).should.eql([6, 4, 3, 2, 2, 1]);
+    sortType([0, 0, 0, 0, 0, 1], desc).should.eql([1, 0, 0, 0, 0, 0]);
+    sortType([0, 0, 0, 0, 0, -1], desc).should.eql([0, 0, 0, 0, 0, -1]);
+  });
 
-	it('should work with (ascending) arrays of objects', () => {
-		sortType(people, objComparator).should.eql([
-  			{"name": 'Passy', "age": 25, "place": 1},
-  			{"name": 'Sindre', "age": 30, "place": 2},
-  			{"name": 'Matt', "age": 35, "place": 3},
-  			{"name": 'Stephen', "age": 40, "place": 4}
+  it('should work with (ascending) arrays of objects', () => {
+    sortType(people, objComparator).should.eql([
+        {"name": 'Passy', "age": 25, "place": 1},
+        {"name": 'Sindre', "age": 30, "place": 2},
+        {"name": 'Matt', "age": 35, "place": 3},
+        {"name": 'Stephen', "age": 40, "place": 4}
       ]);
-	});
+  });
 
   it('should work with random non-sorted arrays', () => {
     let array = _makeRandomArray();
